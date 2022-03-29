24aab505c8f59b1efc670dc07047eb63a24d5445
AytenZeynalova <14rvxpg@code.edu.az>
2022-03-29 04:55:03 +0400

    added

---
+++ 29032022/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace _29032022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("username daxil edin:");
+            string userName = Console.ReadLine();
+
+            User user = new User(userName);
+            while (userName.Length < 6 || userName.Length > 25)
+            {
+                Console.WriteLine("username-in uzunlugu [6-25] olmalidir.yeniden daxil edin:");
+                userName = Console.ReadLine();
+
+            }
+            string password;
+            Console.WriteLine("password daxil edin:");
+            password = Console.ReadLine();
+            user.PassWord = password;
+            while (password.Length < 8 || password.Length> 25)
+            {
+                Console.WriteLine("passwordun uzunlugu [8-25] olmalidir. yeniden daxil edin:");
+                password = Console.ReadLine();
+            }
+
+            Console.WriteLine($"username:{userName} --- password:{password}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #region qaralama
+                //for (int i = 0; i < Username.Length; i++)
+                //{
+                //    if (Username.Length >= 6 && Username.Length <= 25)
+                //    {
+                //        Console.WriteLine($"istifadeci adiniz:{Username}");
+                //    }
+                //    else 
+                //    {
+                //        Console.WriteLine("istifadeci adinin uzunlugu minimum 6 maksimum 25 olmalidir!");
+                //        Console.ReadLine();
+                //    }
+                //}
+
+
+                //Console.WriteLine("password daxil edin");
+                //string password = Console.ReadLine();
+                //for (int i = 0; i < password.Length; i++)
+                //{
+                //    if (password.Length >= 8 && password.Length <= 25)
+                //    {
+                //        for (int j = 0; j < password.Length; j++)
+                //        {
+                //            char c = password[i];
+                //            if (char.IsDigit(c) && char.IsUpper(c) && char.IsLower(c))
+                //            {
+                //                Console.WriteLine("vardir");
+                //            }
+                //            else
+                //            {
+                //                Console.WriteLine("yoxdur");
+                //            }
+
+                //        }
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("yeniden password daxil edin:");
+                //        Console.ReadLine();
+                //    }
+
+                //}
+                #endregion
+
+
+        }
+    }
+}
---
+++ 29032022/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _29032022
+{
+    class User
+    {
+       
+
+        public User(string username)
+        {
+            this.UserName = username;
+          
+        }
+        public string UserName;
+        private string _passWord;
+        public string PassWord
+        {
+            get
+            {
+                return _passWord;
+            }
+
+            set
+            {
+                if (HasDigit(value) && HasUpper(value) && HasLower(value))
+                {
+                    _passWord = value;
+                }
+                else
+                {
+                    Console.WriteLine("password daxil ederken reqemden, boyuk ve balaca herflerden istifade edin:");
+                }
+            }
+        }
+
+
+        //  - HasDigit - parametr olaraq string dəyər qəbul edib o dəyərdə rəqəm varsa geriyə true yoxdusa false qaytaran metod
+        // - HasUpper -  parametr olaraq string dəyər qəbul edib o dəyərdə uppercase varsa geriyə true yoxdursa false qaytaran metod
+        // - HasLower - parametr olaraq string dəyər qəbul edib o dəyərdə lowercase varsa geriyə true yoxdursa false qaytaran metod
+        
+        public bool HasDigit(string exm)
+        {
+            for (int i = 0; i < exm.Length; i++)
+            {
+                if (char.IsDigit(exm[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public bool HasUpper(string exm)
+        {
+            for (int i = 0; i < exm.Length; i++)
+            {
+                if (char.IsUpper(exm[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public bool HasLower(string exm)
+        {
+            for (int i = 0; i < exm.Length; i++)
+            {
+                if (char.IsLower(exm[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+
+    }
+}
